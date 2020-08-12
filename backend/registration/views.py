23309a6474721edbdd3cb5a37e34fa448865f3eb
indradev <yadavdevendra99876@gmail.com>
2020-08-13 00:51:07 +0530

    email verification completed

--- backend/registration/views.py
+++ backend/registration/views.py
@@ -1,44 +1,63 @@
+from django.conf import settings
+from django.core.mail import EmailMultiAlternatives
 from django.core.mail import BadHeaderError, send_mail
 from django.shortcuts import render
 from backend.settings import EMAIL_HOST_USER
 from .models import user_data
-import json 
+from django.template import loader, Context
+import json
 from django.http import JsonResponse
+from django.shortcuts import render
 
-#login
+
+# login
 def login(request):
     if request.method == "POST":
-        data= json.loads(request.body)
-        email= data['email']
-        passwd= data['password']
-        if user_data.objects.filter(email=email, password= passwd):
-            email_db= user_data.objects.get(email=email)
-            if email==email_db:
-                passwd_db=user_data.objects.get(email=email)
-                if passwd==passwd_db:
-                    response="logged in successfully"
+        data = json.loads(request.body)
+        email = data['email']
+        passwd = data['password']
+        if user_data.objects.filter(email=email, password=passwd):
+            email_db = user_data.objects.get(email=email)
+            if email == email_db:
+                passwd_db = user_data.objects.get(email=email)
+                if passwd == passwd_db:
+                    response = "logged in successfully"
                 else:
-                    response="wrong password"
+                    response = "wrong password"
             else:
-                response="wrong mail"
+                response = "wrong mail"
         else:
-            response="you haven't registered yet"
-    return JsonResponse(response ,safe= False)
+            response = "you haven't registered yet"
+    return JsonResponse(response, safe=False)
 
-#register
+# register
+
+
 def register(request):
-    if request.method=="POST":
-        data= json.loads(request.body)
+    if request.method == "POST":
+        data = json.loads(request.body)
         print(data)
-        email_user= data['email']
-        subject="test mail"
-        message="template"
-        send_mail(
-            subject, message, EMAIL_HOST_USER, [email_user],
-            fail_silently=False,
-        )
-    JsonResponse("mail sent" , safe =False)
+        email = data['email']
+        # if user_data.objects.filter(email=email).exists():
+        #     response = "user exists"
+        # else:
+            # user_data.objects.create(**data)
+        response = "user created"
+        subject = "email confirmation"
+        variables = {
+            'email': email,
+            'fname': data['fname'],
+            'lname': data['lname']
 
+        }
+        print(variables)
+        with open(settings.BASE_DIR + "/registration/template/message.txt") as f:
+            text_content =  f.read()
+            html_content = loader.get_template('message.html').render(variables)        
+            msg = EmailMultiAlternatives(subject, text_content, EMAIL_HOST_USER, [email])
+            msg.attach_alternative(html_content, "text/html")
+            msg.send()
+        return JsonResponse(response, safe=False)
 
 
 # def send_mail(request):
