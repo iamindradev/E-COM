775b6ac06c92433469c1450b061f44fce013f453
indradev <yadavdevendra99876@gmail.com>
2020-08-13 00:50:15 +0530

    email confirmation completed

--- backend/registration/models.py
+++ backend/registration/models.py
@@ -2,11 +2,13 @@ from django.db import models
 
 # Create your models here.
 class user_data(models.Model):
-    name = models.CharField(max_length=50)
-    contact = models.CharField(max_length = 150)
-    email = models.EmailField( max_length=254)
-    e_status= models.CharField( max_length=50 , default=True)
+    fname = models.CharField(max_length=50)
+    lname= models.CharField(max_length=50 ,null = True) 
+    contact = models.CharField(max_length = 150, null = True)
+    email = models.EmailField( max_length=254 , null = True)
+    e_status= models.CharField( max_length=50 , default="not verified")
     password = models.CharField(max_length=50)
+
 
     
 
--- frontend/index.js
+++ frontend/index.js
@@ -1,21 +1,30 @@
-var app = angular.module("app" ,[]);
-app.controller("login", function($scope, $http){
-    $scope.log= function(){
-        
+var app = angular.module("app", []);
+app.controller("login", function ($scope, $http) {
+  $scope.submit = function (){
+    $http({
+      method: "POST",
+      url: "http://127.0.0.1:8000/register/",
+      data: {
+        'fname':$scope.fname,
+        'lname':$scope.lname,
+        'email': $scope.mail,
+        'password':$scope.password
+      }
+    }).then(function (res) {
+      console.log(res.data)
+    })
     }
-    
-    $scope.register=function(){
-        
-    }
-    $scope.submit= function(){
-        $http=({
-            method:"POST",
-            url:"http://127.0.0.1:8000/mail_to_user",
-            data:{
-                email:$scope.mail
-            }
-        }).then(function(res){
-            console.log(res.data);
-        })
-    }
-})
+  })
+
+//   $scope.submit=function(){
+//     $http({
+//         method:"POST",
+//         url:"http://6ce91d4cb417.ngrok.io/query/new/",
+//         data:{
+//             'lib_id':id,
+//             'query':$scope.query ,
+//         }
+//     }).then(function(res){
+//         console.log(res.data)
+//     })
+// }
