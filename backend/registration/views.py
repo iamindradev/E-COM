ce41647069f845160b646797746b03ecf82f967c
indradev <yadavdevendra99876@gmail.com>
2020-08-20 00:56:39 +0530

    login completed

--- backend/registration/views.py
+++ backend/registration/views.py
@@ -34,20 +34,36 @@ def onetimepass():
 def login(request):
     if request.method == "POST":
         data = json.loads(request.body)
-        email = data['email']
+        username = data['username']
         passwd = data['password']
-        if user_data.objects.filter(email=email, password=passwd):
-            email_db = user_data.objects.get(email=email)
-            if email == email_db:
-                passwd_db = user_data.objects.get(email=email)
+        if type(username)==int:
+            contact= username
+            if user_data.objects.filter(identity=contact): 
+                passwd_lst = list(user_data.objects.filter(identity=contact).values('password')) 
+                print(passwd_lst)
+                passwd_data=passwd_lst[0]
+                passwd_db=passwd_data['password']
+                print(passwd_db)
                 if passwd == passwd_db:
                     response = "logged in successfully"
                 else:
                     response = "wrong password"
             else:
-                response = "wrong mail"
+                response = "you haven't registered yet or wrong contact"
         else:
-            response = "you haven't registered yet"
+            email = username
+            if user_data.objects.filter(identity=email) :
+                passwd_lst = list(user_data.objects.filter(identity=email).values('password')) 
+                print(passwd_lst)
+                passwd_data=passwd_lst[0]
+                passwd_db=passwd_data['password']
+                print(passwd_db)
+                if passwd == passwd_db:
+                    response = "logged in successfully"
+                else:
+                    response = "wrong password"
+            else:
+                response = "you haven't registered yet or wrong mail id"
     return JsonResponse(response, safe=False)
 
 # register
@@ -56,51 +72,71 @@ def login(request):
 def register(request):
     if request.method == "POST":
         data = json.loads(request.body)
-        print(data)
-        typ = data['typ']
-        if typ == "phone":
-            data = json.loads(request.body)
-            contact = data['identity']
-            url = "https://www.fast2sms.com/dev/bulk"
-            otp = onetimepass()
-
-            payload = "sender_id=FSTSMS&message=THIS%20IS%20YOUR%20OTP%20FOR%20FOR%20CONFIRMATION%20OF%20CONTACT%20NUMBER%20ON%20DEVENDRA'S%20E-CART%20 :" + \
-                otp+"&language=english&route=p&numbers="+contact
-            headers = {
-                'authorization': "VdTeLyCubRJonDXW9wt2m7ENIB5KgcZxUkhOMpiYQ43AHz0fFSk1brgcdECAmMXu4OVQFz95LGUBPxSJ",
-                'Content-Type': "application/x-www-form-urlencoded",
-                'Cache-Control': "no-cache",
-            }
-
-            resp = requests.request("POST", url, data=payload, headers=headers)
-
-            print(resp.text)
-            response = "msg sent"
-
+        identity=data['identity']
+        typ=data['typ']
+        if user_data.objects.filter(identity= identity ).exists() == True:
+            # or user_data.objects.filter(email= identity ).exists()
+            response="user exists"
         else:
-            email = data['identity']
-            response = "user created"
-            subject = "email confirmation"
-            variables = {
-                'email': email,
-                'fname': data['fname'],
-                'lname': data['lname']
+            if typ == "phone":
+                data = json.loads(request.body)
+                contact = data['identity']
+                url = "https://www.fast2sms.com/dev/bulk"
+                otp = onetimepass()
 
-            }
-            print(variables)
-            with open(settings.BASE_DIR + "/registration/template/message.txt") as f:
-                text_content = f.read()
-                html_content = loader.get_template(
-                    'message.html').render(variables)
-                msg = EmailMultiAlternatives(
-                    subject, text_content, EMAIL_HOST_USER, [email])
-                msg.attach_alternative(html_content, "text/html")
-                msg.send()
-                if user_data.objects.filter(email=email).exists():
-                    response = "user exists"
-                else:
+                payload = "sender_id=FSTSMS&message=THIS%20IS%20YOUR%20OTP%20FOR%20CONFIRMATION%20OF%20CONTACT%20NUMBER%20ON%20DEVENDRA'S%20E-CART%20 :" + \
+                    otp+"&language=english&route=p&numbers="+contact
+                headers = {
+                    'authorization': "VdTeLyCubRJonDXW9wt2m7ENIB5KgcZxUkhOMpiYQ43AHz0fFSk1brgcdECAmMXu4OVQFz95LGUBPxSJ",
+                    'Content-Type': "application/x-www-form-urlencoded",
+                    'Cache-Control': "no-cache",
+                }
+                resp = requests.request("POST", url, data=payload, headers=headers)
+                # fname=data['fname']
+                # lname=data['lname']
+                # password=data['password']
+                print(resp.text)
+                user_data.objects.create(otp=otp,**data)
+                response = "msg sent and user created"
+
+            else:
+                email = data['identity']
+                # response = "user created with mail"
+                response= list(user_data.objects.all())
+                print(response)
+                subject = "email confirmation"
+                variables = {
+                    # 'otp':onetimepass(),
+                    'email': email,
+                    'fname': data['fname'],
+                    'lname': data['lname'],
+                    'password':data['password'],
+                    'typ':data['typ']
+
+                }
+                print(variables)
+                with open(settings.BASE_DIR + "/registration/template/message.txt") as f:
+                    text_content = f.read()
+                    html_content = loader.get_template(
+                        'message.html').render(variables)
+                    msg = EmailMultiAlternatives(
+                        subject, text_content, EMAIL_HOST_USER, [email])
+                    msg.attach_alternative(html_content, "text/html")
+                    msg.send()
+                    # user_data.objects.create(email=email,)
                     user_data.objects.create(**data)
+                    
     return JsonResponse(response, safe=False)
+def phoneverify(request):
+    if request.mehtod=="POST":
+        data=json.loads(resquest.body)
+        otp=data['otp']
+        if user_data.objects.filter(otp=otp).exists()==True:
+            response="verified"
+        else:
+            response= "wrong otp"
+    return JsonResponse(response, safe= False)
+
 
 
 # def send_mail(request):
