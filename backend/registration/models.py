9a188af33703fed997a0210448524c5dbe1c01bd
indradev <yadavdevendra99876@gmail.com>
2020-08-20 00:57:47 +0530

    login completed

--- backend/registration/models.py
+++ backend/registration/models.py
@@ -4,10 +4,18 @@ from django.db import models
 class user_data(models.Model):
     fname = models.CharField(max_length=50)
     lname= models.CharField(max_length=50 ,null = True) 
-    contact = models.CharField(max_length = 150, null = True)
-    email = models.EmailField( max_length=254 , null = True)
+    # contact = models.CharField(max_length = 150, null = True)
+    # email = models.EmailField( max_length=254 ,null = True)
+    identity = models.CharField(max_length = 150, null = True)
+    typ= models.CharField(max_length=50 ,null= True)
     e_status= models.CharField( max_length=50 , default="not verified")
     password = models.CharField(max_length=50)
+    otp=models.CharField(max_length=50,null= True)
+
+# class onetimepassword(models.Model):
+#     u= models.ForeignKey("user_data",on_delete="DO_NOTHING", null=True)
+#     otp= models.CharField(max_length=50)
+#     models.DateTimeField( auto_now_add=True)
 
 
     
--- backend/registration/urls.py
+++ backend/registration/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .import views 
+from .import views
 
 urlpatterns = [
-    path('login/', views.login , name="login"), #for the login of users
+    path('login/', views.login, name="login"),  # for the login of users
     # path('mail_to_user/', views.send_mail, name="send_mail"),
-    path('register/', views.register , name="register")# for the registration of user
+    # for the registration of user
+    path('register/', views.register, name="register"),
+    path('confirm/' , views.phoneverify , name =" phone_verify")
 
 ]
